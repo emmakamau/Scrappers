39b86a9eeaa95264c135817a2bad7c0502ecf219
emmakamau <emmakamau@DESKTOP-DRBKMQI.localdomain>
2022-07-18 09:46:43 +0300

    Return data from url

--- jumia/jumia/spiders/info.md
+++ jumia/jumia/spiders/info.md
@@ -1,1 +1,11 @@
 ### CSS Selectors
+
+List of items => response.css('div.itm.col')
+List of articles => response.css('article.prd._box._hvr')
+
+item_link => response.css('a.core::attr(href)').get()
+item_name => response.css('div.name::text').get()
+item_price => response.css('div.prc::text').get()
+item_discount => response.css('div.bdg._dsct::text').get()
+item_categ => response.css('a.core::attr(data-category)').get()
+item_img_url => response.css('img.img::attr(data-src)').get()
--- jumia/jumia/spiders/jumia_hmpg.py
+++ jumia/jumia/spiders/jumia_hmpg.py
@@ -4,7 +4,18 @@ import scrapy
 class JumiaHmpgSpider(scrapy.Spider):
     name = 'jumia_hmpg'
     allowed_domains = ['jumia.co.ke']
-    start_urls = ['http://jumia.co.ke/']
+    start_urls = ['https://www.jumia.co.ke/']
 
     def parse(self, response):
+
+        for item in response.css('article.prd._box._hvr'):
+            yield{
+                'item_link': item.css('a.core::attr(href)').get(),
+                'item_name': item.css('div.name::text').get(),
+                'item_price': item.css('div.prc::text').get(),
+                'item_discount': item.css('div.bdg._dsct::text').get(),
+                'item_category': item.css('a.core::attr(data-category)').get(),
+                'item_img_url': item.css('img.img::attr(data-src)').get()
+            }
+            
         pass
